7714edfe2a3b5251b78a1a265ef10a3eef992b8b
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-08-16 14:16:39 -0300

    webpack revamp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import { Todo } from "./todos.js";
-import "../styles.css";
 let today = new Date().toISOString().slice(0, 10);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
@@ -11,19 +10,12 @@ module.exports = {
     ],
   },
   entry: {
-    index: "./src/modules/index.js",
+    index: "./src/index.js",
   },
   devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "TITLE",
-      template: "./src/index.html",
-    }),
-  ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
   module: {
     rules: [
